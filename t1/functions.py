e65220643482f1b40dcefea46c93cd5b15867057
Juhani <arsmir999@gmail.com>
2021-10-06 00:24:03 +0300

    task1

--- t1/functions.py
+++ t1/functions.py
@@ -24,15 +24,46 @@ def func(data, name):
     print(f'mean: {data.mean()}')
     print(f'variance: {data.var()}')
     print(f'std: {data.std()}')
-    for n in range(4):
-        print(f'central moment n = {n+1}: {stats.moment(data, n+1)}')
+    for n in range(1, 5):
+        print(f'beginning moment n = {n}: {np.mean(data**n)}')
+        print(f'central moment n = {n}: {stats.moment(data, n)}')
     print(f'skewness: {stats.skew(data)}')
     print(f'median: {np.median(data)}')
     print(f'kurtosis: {stats.kurtosis(data, fisher=False)}')
 
-    plt.hist(data)
+    nbins = 10
+    dataMid = (max(data) - min(data))/1
+    c, b = np.histogram(data, bins=nbins)
+    #print(c, b)
+    plt.plot(b[:-1], c)
+    #plt.savefig(f'pol_{name}.png')
+    plt.hist(data, bins=nbins)
+    plt.xlabel('sample')
+    plt.ylabel('frequency')
     plt.savefig(f'hist_{name}.png')
     plt.clf()
+    print(f'file://{os.path.abspath(f"hist_{name}.png")}')
+
+    dict = {}
+    for i in data:
+        if i in dict:
+            dict[i] += 1 / len(data)
+        else:
+            dict[i] = 1 / len(data)
+
+    dict2 = {}
+    for i in dict.keys():
+        dict2[i] = 0
+        for j in dict2.keys():
+            dict2[i] += dict[j]
+
+    plt.plot(sorted(dict2.keys()), sorted(dict2.values()))
+    plt.xlabel('sample')
+    plt.ylabel('prob')
+    plt.savefig(f'probFunc_{name}.png')
+    plt.clf()
+    print(f'file://{os.path.abspath(f"probFunc_{name}.png")}')
+
     print('interval 95%: {}'.format(
     stats.t.interval(0.95, len(data)-1, loc=np.mean(data),scale=stats.sem(data))
     ))
