5d1965d4f8e0a0d000c56e91c24814a867c36d64
Juhani <arsmir999@gmail.com>
2021-11-24 01:37:27 +0300

    t6

--- t6/file.py
+++ t6/file.py
@@ -8,7 +8,8 @@ from tex import tex
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-
+tex.printhead()
+tex.section('Компонентный анализ')
 filename = '../t4/data.xlsx'
 file = pd.ExcelFile(filename)
 sheets = file.sheet_names[1:]
@@ -17,36 +18,68 @@ data = [parsesheet(file, sheet) for sheet in sheets]
 m = min([len(d) for d in data])
 data = [d[:m] for d in data]
 #print(data)
-
+tex.section('Анализ данных')
 for d in data:
     plt.plot(d)
 plt.legend(sheets)
-#plt.show()
+plt.title('Uncompressed data')
+plt.savefig('data.png')
+tex.addimage('data.png')
+plt.clf()
 
 pca = PCA()
 pca.fit(data)
-print(np.round(pca.explained_variance_ratio_, 5))
-print([round(x) for x in pca.singular_values_])     #not working properly
+tex.printline('Важность')
+tex.plaintext(dictstr2(dict(zip(sheets,
+np.round(pca.explained_variance_ratio_, 5)))))
+tex.printline('Вклады в суммарную дисперсию')
+tex.plaintext(dictstr2(dict(zip(sheets,
+[round(x) for x in pca.singular_values_]))))     #not working properly
 plt.plot(sheets ,pca.singular_values_)
+plt.xticks(sheets, rotation='vertical')
+plt.subplots_adjust(bottom=0.3)
 plt.xlabel('eingvalues')
 plt.ylabel('value')
-#plt.show()
+plt.savefig('vals1.png')
+tex.addimage('vals1.png')
+plt.clf()
+
 for d in pca.fit_transform(data):
     plt.plot(d)
 plt.legend(sheets)
-#plt.show()
+plt.title('Compressed data')
+plt.savefig('compressed.png')
+tex.addimage('compressed.png')
 plt.clf()
-print(pca.explained_variance_)
+tex.plaintext(dictstr2(dict(zip(sheets,
+[round(x) for x in pca.explained_variance_]))))
 #print(pca.n_features_in_)
 
+
+#print(pca.score(data))
+#print(np.round(pca.explained_variance_ratio_, 5))
+
+tex.section('Анализ cвязи с Y')
 
 dataY = [parsesheet(file, sheet) for sheet in file.sheet_names]
 m = min([len(d) for d in dataY])
 dataY = [d[:m] for d in dataY]
 pca = PCA()
 pca.fit(dataY)
-print(np.round(pca.explained_variance_ratio_, 5))
-print([round(x) for x in pca.singular_values_])
+tex.printline('Важность')
+tex.plaintext(dictstr2(dict(zip(file.sheet_names,
+np.round(pca.explained_variance_ratio_, 5)))))
+tex.printline('Вклады в суммарную дисперсию')
+tex.plaintext(dictstr2(dict(zip(file.sheet_names,
+[round(x) for x in pca.singular_values_]))))
 plt.plot(file.sheet_names ,pca.singular_values_)
-plt.show()
+plt.xticks(file.sheet_names, rotation='vertical')
+plt.subplots_adjust(bottom=0.3)
+plt.savefig('vals2.png')
+tex.addimage('vals2.png')
 plt.clf()
+tex.printline('Зависимость')
+compare(dataY[0], dataY[1])
+
+
+tex.printend()
--- t6/functions.py
+++ t6/functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy import stats, optimize
+from scipy import stats#, optimize
 import matplotlib.pyplot as plt
 import os, sys#, inspect
 sys.path.append('../common/')
@@ -20,3 +20,32 @@ def parsesheet(file, sheet):
     data = np.array(data)
     data = data[~pd.isnull(data)]
     return data
+
+def dictstr2(dict):
+    res = "{:<15} {:<10}\n".format('NAME', 'VALUE')
+    for key, value in dict.items():
+        val = value
+        res += "{:<15} {:<10}\n".format(key, val)
+    return res
+
+def compare(datax, datay, a=0.1):
+    if len(datax) != len(datay):
+        x = min([len(datax), len(datay)])
+        datax = datax[:x]
+        datay = datay[:x]
+    tex.printline(f'a={a}')
+    ks = stats.ks_2samp(datax, datay)
+    tex.plaintext(f'Kolmogorov-Smirnov: {stats.ks_2samp(datax, datay)}')
+    checkcor(a, ks)
+    wil = stats.wilcoxon(datax, datay)
+    tex.plaintext(f'Wilcoxon: {wil}')
+    checkcor(a, wil)
+    kw = stats.kruskal(datax, datay)
+    tex.plaintext(f'Kruskal-Wallis: {kw}')
+    checkcor(a, kw)
+
+def checkcor(a, crit):
+    if a > crit[1]:
+        tex.plaintext(f'Independent')
+    else:
+        tex.plaintext(f'Dependent')
